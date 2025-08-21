f3c8251f670d3777a694deffdd9345898a89a230
Rebeca Elizabeth Castillo <Rebe_Pulunto@outlook.com>
2025-08-21 10:08:10 -0600

    Initial commit

---
+++ src/main/java/org/esfe/sisLabZetino/SisLabZetinoApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.sisLabZetino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisLabZetinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisLabZetinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/sisLabZetino/SisLabZetinoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.sisLabZetino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisLabZetinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
